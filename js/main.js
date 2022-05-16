b9378cee68d86ce7485995f02de7451b8d16890f
mokbulhussain <mokbulhussain2@gmail.com>
2022-05-16 20:45:28 +0600

    responsive upload

--- js/main.js
+++ js/main.js
@@ -23,8 +23,8 @@
           {
             breakpoint: 600,
             settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
+              slidesToShow: 1,
+              slidesToScroll: 1
             }
           },
           {
